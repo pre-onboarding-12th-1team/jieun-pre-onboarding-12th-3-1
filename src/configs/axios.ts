08e0e6cf11a218e3f9544f6e206dc9fdb6964916
jieeeun2 <jieun2.dev@gmail.com>
2023-09-06 09:20:27 +0900

    feat: API호출할때마다 console.info() 출력을 통해 콘솔창에서 API호출횟수 확인

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,10 +1,23 @@
 import axios from 'axios'
+import type { InternalAxiosRequestConfig } from 'axios'
+import { setSessionStorage } from './sessionStorage';
 
 let production = false
 
 export const axiosInstance = axios.create({
   baseURL: production ? '' : 'http://localhost:4000'
 });
+
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig<any>) => {
+    console.info("calling api")
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 axiosInstance.interceptors.response.use(
   (response) => {
